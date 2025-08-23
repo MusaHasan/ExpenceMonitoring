53e45e0582f461e3ae08f686350eee2d0c6824f6
musahasan2025@gmail.com <musahasan2025@gmail.com>
2025-08-23 22:21:00 +0600

    Creating deploy suptree branch

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+	base: '/ExpenceManagement/',
 	plugins: [vue()],
 	define: {
 		'process.env': {},
